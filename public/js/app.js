5b87800cece1e492a20d8da8a3ab09bf37676f6b
Aniss Aziz <anissaziz31000@gmail.com>
2024-12-17 14:51:44 +0100

    feat: add footer dark mode

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,17 @@
 var icon = document.getElementById("icon");
 var logo = document.querySelector(".navbar img")
+var logo_footer = document.querySelector(".my-footer img" )
 
 icon.onclick = function(){
     document.body.classList.toggle("dark-theme");
     if (document.body.classList.contains("dark-theme")) {
         logo.src = "images/logo-blanc.png";
+        logo_footer.src = "images/logo-blanc.png";
+        icon.style.filter = "invert(1)"
     }
     else {
         logo.src = "images/logonoir.png";
+        logo_footer.src = "images/logo-noir.png";
+        icon.style.filter = "contrast(0.3)";
     }
 }
